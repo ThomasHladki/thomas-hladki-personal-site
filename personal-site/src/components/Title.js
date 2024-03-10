ef887ec022fa1f54e632c1c848d3583db03ba9c5
Thomas Hladki <tomhladki@icloud.com>
2024-03-10 03:48:23 -0400

    Styling

--- personal-site/src/components/Title.js
+++ personal-site/src/components/Title.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 const Title = ({ name, headshotImgSrc, }) => {
     return (
-        <div className="bg-stone-800 w-1/2 flex justify-center rounded-3xl h-96 m-10 p-16 z-10 shadow-outline">
+        <div className="bg-stone-800 w-1/2 flex justify-center rounded-3xl h-96 m-10 p-16 z-10 shadow-outline border-8 border-zinc-950">
             <div className="flex flex-col items-center">
                 <h1 className="text-5xl font-bold underline text-gray-50 text-outline mb-8">
                     {name}
@@ -10,7 +10,7 @@ const Title = ({ name, headshotImgSrc, }) => {
                 <img src={headshotImgSrc} alt="headshot" className="text-sm max-h-36 max-w-36 rounded-full"></img>
             </div>
             <div>
-                
+
             </div>
 
 
